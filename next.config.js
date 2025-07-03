f71f6e8bb5bf5a6795482aa59c454d03bd6a21b1
Tanishkagimhan <tanishkagimhan4@gmail.com>
2025-07-03 16:36:29 +0530

    second commmit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // Remove 'appDir' unless you're using a custom app directory and it's still supported
+  },
+  // swcMinify: true, // REMOVE this line
+};
+
+module.exports = nextConfig;
---
+++ src/common/widgets/Socials/config/constans.tsx
@@ -0,0 +1,12 @@
+import { FiGithub, FiLinkedin, FiTwitter, FiInstagram, FiFacebook } from 'react-icons/fi'
+import { SocialTypes } from './types'
+
+
+// social media data
+export const socialData : SocialTypes[] = [
+    { icon : FiGithub, url : "github.com/raihanhosen011" },
+    { icon : FiLinkedin, url : "www.linkedin.com/in/raihanhosen01/" },
+    { icon : FiTwitter, url : "twitter.com/raihanhosen01" },
+    { icon : FiInstagram, url : "instagram.com/raihanhosen0001" },
+    { icon : FiFacebook, url : "www.facebook.com/raihanhosen01" },
+]
